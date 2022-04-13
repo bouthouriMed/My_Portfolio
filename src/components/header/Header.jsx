f21545f945f90124dce35fc927e8006e27a8b53e
bouthouriMed <bouthourimed76@gmail.com>
2022-04-13 01:35:39 +0100

    Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import CTA from './CTA'
+import ME from '../../assets/me.png';
+import HeaderSocials from './HeaderSocials';
+
+import "./header.css"
+
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Bouthouri Mohamed</h1>
+        <h5 className='text-light'>Full-Stack software developer</h5>
+        <CTA/>
+
+        <div className="me">
+          <img src={ME} alt="Me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll down</a>
+
+        <HeaderSocials/>
+      </div>
+    </header>
   )
 }
 
